10805fe4f72ba7e7e2faa45fe3f4dc5e2f4c08ef
Yazan <yazann.abbas@gmail.com>
2021-03-21 18:48:14 +0100

    Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 // import Footer from "./Footer";
-// import Header from "./Header";
+import Header from "./Header";
 // import Note from "./Note";
 // import Notes from "../Notes"
 
 function App() {
   return (
     <div >
-    <h1>Hello world!</h1>
+    <Header/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
-// import React from "react";
+import React from "react";
+
+function Header(){
+    return <header>Notes App</header> 
+}
 
 
 
 
-
-
-// export default Header;
+export default Header;
