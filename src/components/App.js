eb7ac12a066950b757c4c05ddef04381005a2793
Yazan <yazann.abbas@gmail.com>
2021-03-21 18:25:44 +0100

    test React

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import Footer from "./Footer";
+// import Header from "./Header";
+// import Note from "./Note";
+// import Notes from "../Notes"
+
+function App() {
+  return (
+    <div >
+    <h1>Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
